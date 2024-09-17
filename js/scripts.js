7cbfc9abe7b24ef43788d2fe596882b49c7031b7
Claire <cfeltgungatan@gmail.com>
2024-09-16 20:28:13 -0600

    Almost Full project

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menuLinks');
+
+hamburgerElement.addEventListener('click', () => {
+    hamburgerElement.classList.toggle('open');
+    navElement.classList.toggle('open');
+});
+console.log ('hiiiii')
